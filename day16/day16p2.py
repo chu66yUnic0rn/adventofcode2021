0421ad1caa4df4e8842ea5b85b1d03fc11720f40
Mina <min4zh0u@gmail.com>
2021-12-24 00:02:21 -0500

    day 16

--- day16/day16p2.py
+++ day16/day16p2.py
@@ -1,10 +1,9 @@
-versions = []
+import math
 
 def determine_packet(binary_value, pos):
 	print(binary_value[pos:])
 	packet_version = int(binary_value[pos:pos+3],2)
 	print("version num: " + str(packet_version))
-	versions.append(packet_version)
 	packet_type = int(binary_value[pos+3:pos+6],2)
 	# print(packet_type)
 	if packet_type == 4:
@@ -12,14 +11,38 @@ def determine_packet(binary_value, pos):
 		print(result)
 		return result
 	else:
-		result = operator_val(binary_value, pos+6)
+		result, pos = operator_val(binary_value, pos+6)
 		print(result)
-		return result
+		if packet_type == 0:
+			return sum(result), pos
+		elif packet_type == 1:
+			return math.prod(result), pos
+		elif packet_type == 2:
+			return min(result), pos
+		elif packet_type == 3:
+			return max(result), pos
+		elif packet_type == 5:
+			if result[0] > result[1]:
+				return 1, pos
+			else:
+				return 0, pos
+		elif packet_type == 6:
+			if result[0] < result[1]:
+				return 1, pos
+			else:
+				return 0, pos
+		elif packet_type == 7:
+			if result[0] == result[1]:
+				return 1, pos
+			else:
+				return 0, pos
+
 
 def literal_val(binary_value, pos):
 	print("literal")
 	length = 0
 	values = []
+	# literal_values = binary_value[pos:]
 	while binary_value[pos] != "0":
 		values.append(binary_value[pos+1:pos+5])
 		pos += 5
@@ -28,11 +51,13 @@ def literal_val(binary_value, pos):
 		values.append(binary_value[pos+1:pos+5])
 		pos += 5
 		length += 5
+		# value = int(''.join(values),2)
 	return int(''.join(values),2), pos
 
 def operator_val(binary_value, pos):
 	print("operator")
 	length_type_id = binary_value[pos]
+	# operator_values = binary_value[pos+7:]
 	values = []
 
 	if length_type_id == '0':
@@ -58,13 +83,14 @@ def operator_val(binary_value, pos):
 	return values, pos
 
 
-# line = "8A004A801A8002F478"
-# line = "D2FE28"
-# line = "38006F45291200"
-# line = "EE00D40C823060"
-# line = "620080001611562C8802118E34"
-# line = "A0016C880162017C3686B18A3D4780"
-# line = "C0015000016115A2E0802F182340"
+# line = "C200B40A82"
+# line = "04005AC33890"
+# line = "880086C3E88112"
+# line = "CE00C43D881120"
+# line = "D8005AC2A8F0"
+# line = "F600BC2D8F"
+# line = "9C005AC2F8F0"
+# line = "9C0141080250320F1802104A08"
 
 with open("day16_input") as f:
 	line = f.read().strip()
@@ -74,4 +100,4 @@ with open("day16_input") as f:
 		binary_value += str(bin(int(char,16)))[2:].zfill(4)
 
 	print(determine_packet(binary_value, 0))
-	print(versions, sum(versions))
+# print(versions, sum(versions))
