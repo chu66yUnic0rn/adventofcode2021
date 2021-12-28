79ce6b829cf247f433240cd9f25af284a24e3ede
Mina <min4zh0u@gmail.com>
2021-12-28 18:35:01 -0500

    day 4 normal solution

--- day4/day4p1.py
+++ day4/day4p1.py
@@ -1,37 +1,61 @@
 from collections import Counter
 
 class Board:
-    def __init__(self):
-        self.numbers = []
-        self.hits = []
+	def __init__(self):
+		self.numbers = {}
+		self.hits = []
 
-    def add_numbers(self, numbers):
-        self.numbers.append(numbers)
+	def add_numbers(self, row, numbers):
+		self.numbers.update({row:numbers})
 
-    def find_hits(self, draw):
-        tmp_vertical = []
-        for number_line in numbers:
-            for index, num in enumerate(number_line.split(" ")):
-                if draw == num:
-                    self.hits.append(number_line)
-                    for i in range(5):
-                        tmp_vertical.append(self.numbers[i].split(" ")[index])
-                    self.hits.append(" ".join(tmp_vertical))
-        top_hit = Counter(self.hits).most_common(1)
-        if top_hit[0][1] == 5:
-            return top_hit[0][0]
-        return None
+	def find_hits(self, draw):
+		for row, nums in self.numbers.items():
+			if draw in nums:
+				index = nums.index(draw)
+				if (row, index) not in self.hits:
+					self.hits.append((row,index))
+					row_hit = Counter(hit[0] for hit in self.hits)
+					index_hit = Counter(hit[1] for hit in self.hits)
+					if row_hit.most_common(1)[0][1] == 5 or index_hit.most_common(1)[0][1] == 5:
+						return self
+		return None
+
+	def calculate_score(self, last_called):
+		unmarked = 0
+		for row, nums in self.numbers.items():
+			for i in range(len(nums)):
+				if (row,i) not in self.hits:
+					unmarked += int(nums[i])
+		print(unmarked)
+		return unmarked*int(last_called)
 
 with open("day4_input") as f:
-    lines = f.readlines()
-    draws = lines[0].strip().split(',')
-    lines.pop(0)
-    lines.pop(0)
-    lines.append("\n")
-    boards = []
-    for i in range(0, len(lines), 6):
-        new_board = Board()
-        new_board.add_numbers(lines[i])
+	lines = f.readlines()
+	draws = lines[0].strip().split(',')
+	lines.pop(0)
+	lines.pop(0)
+	lines.append("\n")
+	boards = []
+	for i in range(0, len(lines), 6):
+		new_board = Board()
+		for x in range(5):
+			new_board.add_numbers(x,lines[i+x].split())
+		boards.append(new_board)
+		print(new_board.numbers.items())
+	# print(boards)
+	for draw in draws:
+		print(draw)
+		for board in boards:
+			result = board.find_hits(draw)
+			if result is not None:
+				print(result.numbers)
+				print(result.calculate_score(draw))
+				break
+		else:
+			continue
+		break
+
+
 
 
 
